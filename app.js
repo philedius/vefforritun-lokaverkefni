ff162becf0e1c149c275e9fad0bcbc342acd4e9e
Fannar Freyr <fannarr@gmail.com>
2015-11-26 21:59:08 +0000

    port fix fyrir heroku

--- app.js
+++ app.js
@@ -17,9 +17,6 @@ app.get('/', function(request, response) {
 //   console.log('Node app is running on port', app.get('port'));
 // });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
